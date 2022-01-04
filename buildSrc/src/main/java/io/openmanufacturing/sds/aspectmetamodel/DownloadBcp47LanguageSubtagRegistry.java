d1d74a6f1f4dd9bd364843f8b91e8b638f3a9215
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2022-01-04 14:22:20 +0100

    Add missing copyright header

--- buildSrc/src/main/java/io/openmanufacturing/sds/aspectmetamodel/DownloadBcp47LanguageSubtagRegistry.java
+++ buildSrc/src/main/java/io/openmanufacturing/sds/aspectmetamodel/DownloadBcp47LanguageSubtagRegistry.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2022 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * See the AUTHORS file(s) distributed with this work for additional
+ * information regarding authorship.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 package io.openmanufacturing.sds.aspectmetamodel;
 
 import java.io.File;
