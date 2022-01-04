0a87483c6d6fe805fe27541e10ed0e4454e14dcc
Schmidt-Dumont Georg (BCI/ESW17) <sgh4fe@bosch.com>
2022-01-04 09:51:36 +0100

    Fix Language Constraint error message
    fixes #85

--- src/test/java/io/openmanufacturing/sds/aspectmetamodel/LanguageConstraintShapeTest.java
+++ src/test/java/io/openmanufacturing/sds/aspectmetamodel/LanguageConstraintShapeTest.java
@@ -58,7 +58,7 @@ public class LanguageConstraintShapeTest extends AbstractShapeTest {
       final BammUrns bammUrns = new BammUrns( metaModelVersion );
       final String focusNode = TEST_NAMESPACE_PREFIX + "TestLanguageConstraintInvalidLanguageCode";
 
-      final SemanticError result = new SemanticError( "The language code must be a valid according to ISO 639 1.", focusNode,
+      final SemanticError result = new SemanticError( "The language code must be a valid according to ISO 639-1.", focusNode,
             bammUrns.languageCodeUrn, VIOLATION_URN, "DE_de" );
       expectSemanticValidationErrors( "language-constraint-shape", "TestLanguageConstraintInvalidLanguageCode", metaModelVersion, result );
    }
