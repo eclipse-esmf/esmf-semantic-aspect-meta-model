be5bb63b1476bf586208e2d6378892142b436369
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2022-04-01 16:31:20 +0200

    Remove yaml dependency that is not required any more

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,6 @@ dependencies {
     implementation("org.apache.jena:jena-core:4.2.0")
     implementation("org.apache.jena:jena-arq:4.2.0")
     implementation("org.apache.commons:commons-text:1.9")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
 }
 
 repositories {
