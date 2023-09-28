0bbb901700736b6825b442ea55c07c2a07010173
Yuriy Shevtsov <external.iurii.shevtsov@bosch.com>
2023-09-28 09:28:03 +0200

    Added validation exampleValues against regex #384

--- esmf-semantic-aspect-meta-model/src/test/java/org/eclipse/esmf/samm/RegularExpressionConstraintShapeTest.java
+++ esmf-semantic-aspect-meta-model/src/test/java/org/eclipse/esmf/samm/RegularExpressionConstraintShapeTest.java
@@ -13,6 +13,8 @@
 
 package org.eclipse.esmf.samm;
 
+import java.util.Map;
+
 import org.apache.jena.vocabulary.XSD;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,10 +23,12 @@ import org.eclipse.esmf.samm.validation.SemanticError;
 
 public class RegularExpressionConstraintShapeTest extends AbstractShapeTest {
 
+   public static final String SPEC_PATH = "regular-expression-constraint-shape";
+
    @ParameterizedTest
    @MethodSource( value = "allVersions" )
    public void testRegularExpressionConstraintValidationExpectSuccess( final KnownVersion metaModelVersion ) {
-      checkValidity( "regular-expression-constraint-shape", "TestRegularExpressionConstraint", metaModelVersion );
+      checkValidity( SPEC_PATH, "TestRegularExpressionConstraint", metaModelVersion );
    }
 
    @ParameterizedTest
@@ -36,7 +40,7 @@ public class RegularExpressionConstraintShapeTest extends AbstractShapeTest {
 
       final SemanticError resultForRegularExpression = new SemanticError(
             messageInvalidRegularExpression, focusNode, sammUrns.valueUrn, violationUrn, "(" );
-      expectSemanticValidationErrors( "regular-expression-constraint-shape",
+      expectSemanticValidationErrors( SPEC_PATH,
             "TestRegularExpressionConstraintWithInvalidRegularExpression",
             metaModelVersion, resultForRegularExpression );
    }
@@ -51,7 +55,37 @@ public class RegularExpressionConstraintShapeTest extends AbstractShapeTest {
       final SemanticError resultForDataType = new SemanticError( expectedMessage,
             focusNode, "", violationUrn, XSD.xboolean.getURI() );
       expectSemanticValidationErrors(
-            "regular-expression-constraint-shape", "TestRegularExpressionConstraintWithInvalidType",
+            SPEC_PATH, "TestRegularExpressionConstraintWithInvalidType",
             metaModelVersion, resultForDataType );
    }
+
+   @ParameterizedTest
+   @MethodSource( value = "versionsStartingWith2_0_0" )
+   public void testValidExampleValuesValidation( final KnownVersion metaModelVersion ) {
+      checkValidity( SPEC_PATH, "TestPropertyWithExampleValues", metaModelVersion );
+   }
+
+   @ParameterizedTest
+   @MethodSource( value = "versionsStartingWith2_0_0" )
+   public void testInvalidExampleValuesValidation( final KnownVersion metaModelVersion ) {
+      final String aspect = "TestPropertyWithInvalidExampleValues";
+      final String focusNode = testNamespacePrefix + "TestRegularExpressionConstraint";
+      final String expectedMessage = validator.getMessageText(
+            "samm-c:RegularExpressionConstraintShape",
+            "ERR_WRONG_EXAMPLE_VALUE",
+            metaModelVersion );
+      final SemanticError resultForDataType = new SemanticError( expectedMessage,
+            focusNode, "", violationUrn, focusNode )
+            .withParams( Map.of(
+                  "{?property}",testNamespacePrefix + "InvalidRegexProperty",
+                  "{?exampleValue}", "-ABC123456-000",
+                  "{?regex}", "^[A-Z]{3}\\d{6}-\\d{3}$"
+            ));
+      expectSemanticValidationErrors(
+            SPEC_PATH,
+            aspect,
+            metaModelVersion,
+            resultForDataType );
+   }
+
 }
