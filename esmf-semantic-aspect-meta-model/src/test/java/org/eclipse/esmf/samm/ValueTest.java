5571750d735a9a2d6d90c6655f5683a1d1bc2422
Yauhenikapl <external.Yauheni.Kapliarchuk@de.bosch.com>
2025-02-28 11:35:30 +0300

    Update value min count

--- esmf-semantic-aspect-meta-model/src/test/java/org/eclipse/esmf/samm/ValueTest.java
+++ esmf-semantic-aspect-meta-model/src/test/java/org/eclipse/esmf/samm/ValueTest.java
@@ -1,5 +1,7 @@
 package org.eclipse.esmf.samm;
 
+import org.eclipse.esmf.samm.validation.SemanticError;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -7,31 +9,40 @@ class ValueTest extends AbstractShapeTest {
 
    @ParameterizedTest
    @MethodSource( value = "versionsStartingWith2_2_0" )
-   void defaultValueDefinition ( final KnownVersion metaModelVersion ) {
+   void defaultValueDefinition( final KnownVersion metaModelVersion ) {
       checkValidity( "value-shape", "DefaultValue", metaModelVersion );
    }
 
    @ParameterizedTest
+   @MethodSource( "versionsStartingWith2_2_0" )
+   void invalidValueDeclarationExpectedFailure( KnownVersion metaModelVersion ) {
+      final SemanticError expectedError = new SemanticError( "Property needs to have at least 1 values, but found 0",
+            testNamespacePrefix + "GreenLight", "urn:samm:org.eclipse.esmf.samm:meta-model:2.2.0#value", violationUrn, "" );
+
+      expectSemanticValidationErrors( "value-shape", "InvalidValueDeclaration", metaModelVersion, expectedError );
+   }
+
+   @ParameterizedTest
    @MethodSource( value = "versionsStartingWith2_2_0" )
-   void valueWithEnumeration ( final KnownVersion metaModelVersion ) {
+   void valueWithEnumeration( final KnownVersion metaModelVersion ) {
       checkValidity( "value-shape", "ValueWithEnumeration", metaModelVersion );
    }
 
    @ParameterizedTest
    @MethodSource( value = "versionsStartingWith2_2_0" )
-   void multipleValueWithEnumeration ( final KnownVersion metaModelVersion ) {
+   void multipleValueWithEnumeration( final KnownVersion metaModelVersion ) {
       checkValidity( "value-shape", "MultipleValueWithEnumeration", metaModelVersion );
    }
 
    @ParameterizedTest
    @MethodSource( value = "versionsStartingWith2_2_0" )
-   void referenceValueWithProperty ( final KnownVersion metaModelVersion ) {
+   void referenceValueWithProperty( final KnownVersion metaModelVersion ) {
       checkValidity( "value-shape", "ReferenceValueWithProperty", metaModelVersion );
    }
 
    @ParameterizedTest
    @MethodSource( value = "versionsStartingWith2_2_0" )
-   void anonymousDescribedValueInExampleValue ( final KnownVersion metaModelVersion ) {
+   void anonymousDescribedValueInExampleValue( final KnownVersion metaModelVersion ) {
       checkValidity( "value-shape", "AnonymousDescribedValueInExampleValue", metaModelVersion );
    }
 }
